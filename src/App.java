4fcc4cdcfb9b956a085a9f2fe36d34a57feecb55
Amice Baptiste <amice.bap@gmail.com>
2023-11-28 11:44:44 +0100

    file input

--- src/App.java
+++ src/App.java
@@ -1,3 +1,6 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 import org.antlr.runtime.ANTLRStringStream;
 import org.antlr.runtime.CommonTokenStream;
 import org.antlr.runtime.tree.CommonTree;
@@ -5,14 +8,39 @@ import org.antlr.runtime.tree.CommonTree;
 public class App {
     public static void main(String[] args) throws Exception {
 
+        // Check if a file name is provided as argument
+        if(args == null || args.length != 1) {
+            System.out.println("Please provide a file name as argument");
+            System.exit(1); 
+        }
+
+        // Read the file // todo put it in src
+        try {
+            Path path = Path.of(args[0]);
+            String src = Files.readString(path);
+            System.out.println(src);
+        } catch (Exception e) {
+            System.out.println("Error while reading file");
+            System.exit(1);
+        }
+
+        // temp test
         String src = "function true : read % Result := (cons nil nil) % write Result";
+
+        // Parse the file content
         while_astLexer lexer = new while_astLexer(new ANTLRStringStream(src));
+        // Get the token stream from the lexer
         CommonTokenStream tokens = new CommonTokenStream(lexer);
+        // Create the parser
         while_astParser parser = new while_astParser(tokens);
 
+        // Call the start rule, which is the entry point of the grammar
         while_astParser.startProgram_return startProgram = parser.startProgram();
+
+        // The root of the AST
         final CommonTree treeRoot = (CommonTree) startProgram.getTree();
-        CommonTree currentNode = (CommonTree) treeRoot.getChild(0);
+
+        CommonTree currentNode = (CommonTree) treeRoot;
 
         
     }   

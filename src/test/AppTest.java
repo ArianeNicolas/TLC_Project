3319d61593da6ce6818ca7cc6c8092e1851c8b34
Amice Baptiste <amice.bap@gmail.com>
2023-12-21 08:32:14 +0100

    ajout tests

--- src/test/AppTest.java
+++ src/test/AppTest.java
@@ -22,9 +22,8 @@ public class AppTest {
 
 
     @Test
-    public void inlinedExample() throws RecognitionException {
-        String src = "function sub : read Op1, Op2 % Resul) od % write Result; ";
-        System.out.println("Source code: " + src); 
+    public void testInlinedWorkingExample() throws RecognitionException {
+        String src = "function sub : read Op1, Op2 % Result := Op1; foreach Op in Op2 do  Result := (tl Result) od % write Result;";
 
         // Parse the file content
         while_astLexer lexer = new while_astLexer(new ANTLRStringStream(src));
@@ -34,23 +33,62 @@ public class AppTest {
         // Create the parser
         while_astParser parser = new while_astParser(tokens);
 
-
-        PrintStream outputText = new PrintStream(System.out);
-        System.setOut(outputText );
         // Call the start rule, which is the entry point of the grammar
         while_astParser.startProgram_return startProgram = parser.startProgram();
 
+        // count the number of errors
+        int nbErrors = parser.getNumberOfSyntaxErrors();
 
-
-
-        assert(true);
-        
-
+        assert(nbErrors == 0);
     }
 
     @Test
-    public void testAdd() {
-        assert(true);
+    public void testInlinedInvalidExample() throws RecognitionException {
+        String src = "function sub do  Result := (tl Result) od % write Result;";
+
+        // Parse the file content
+        while_astLexer lexer = new while_astLexer(new ANTLRStringStream(src));
+        // Get the token stream from the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        // Create the parser
+        while_astParser parser = new while_astParser(tokens);
+
+        // Call the start rule, which is the entry point of the grammar
+        while_astParser.startProgram_return startProgram = parser.startProgram();
+
+        // count the number of errors
+        int nbErrors = parser.getNumberOfSyntaxErrors();
+
+        assert(nbErrors > 0);
+    }
+
+    @Test
+    public void testAddFunction() throws RecognitionException {
+        String src = "AAAAAAAA";
+        // Read the file // todo put it in src
+        try {
+            Path path = Path.of("while_exemples/add.while"); 
+            src = Files.readString(path); 
+        } catch (Exception e) {
+            System.out.println("Error while reading file");
+        }
+
+        // Parse the file content
+        while_astLexer lexer = new while_astLexer(new ANTLRStringStream(src));
+        // Get the token stream from the lexer
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+
+        // Create the parser
+        while_astParser parser = new while_astParser(tokens);
+
+        // Call the start rule, which is the entry point of the grammar
+        while_astParser.startProgram_return startProgram = parser.startProgram();
+
+        // count the number of errors
+        int nbErrors = parser.getNumberOfSyntaxErrors();
+
+        assert(nbErrors == 0);
     }
     
 }
